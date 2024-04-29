8c01eb437bd3c0bd4b6a2e24506f84af35481c5d
Ming Li <Mingrkli@gmail.com>
2024-04-28 21:33:03 -0700

    The remaining files

---
+++ src/test/java/com/springlearning/newticket2/NewTicket2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springlearning.newticket2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewTicket2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
